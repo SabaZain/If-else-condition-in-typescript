c2c2684be5c3521d744c37ebcc69700d1beac9d6
Saba Zain <159971290+SabaZain@users.noreply.github.com>
2024-04-20 17:21:51 +0500

    Adding my practise work of If-else condition in typescript

---
+++ main.js
@@ -0,0 +1,66 @@
+// if else condition
+// Example 1:
+// let weather = "rainy";
+// if(weather == "cloudy"){
+//     console.log("Wear a light jacket")
+// } else if(weather == "rainy"){
+//     console.log("Wear a rain coat or take an umbrella")
+// } else if(weather == "windy"){
+//     console.log("Wear a cover")
+// } else {
+//     console.log("Wear sunglasses")
+// }
+// Example 2:
+// let percentage = 50
+// if(percentage >90 && percentage <= 100){
+//     console.log("Your Grade is A+")
+// } else if(percentage >80 && percentage <=90){
+//     console.log("Your Grade is A")
+// } else if(percentage >70 && percentage <=80){
+//     console.log("Your Grade is B+")
+// } else if(percentage >60 && percentage <=70){
+//     console.log("Your Grade is B")
+// } else if(percentage >50 && percentage <=60){
+//     console.log("Your Grade is C")
+// } else {
+//     console.log("Your Status is Failed")
+// }
+// Example 3:
+// let userName : string = "Bilal"
+// let password : string = "1505"
+// let pinCode : string = "234"
+// if(userName == "Salman" && password == "1507" ){
+//     console.log("Kindly provide pin code")
+//     if(pinCode == "123"){
+//         console.log("Welcome Home" + " " + userName)
+//     } else {
+//         console.log("Invalid pin code")
+//     } 
+// } else {
+//     console.log("Invalid User")
+// } 
+// Example 4:
+// let name : string = "Ali"
+// if(name == "Ali"){
+//     console.log("Welcome Home")
+// } else {
+//     console.log("Sorry!!!")
+// }
+// Example 5:
+// let name : string = "Saba"
+// let age : number = 27
+// if (name == "Sana" || age == 27){
+//     console.log("Congratulations! You are eligible")
+// } else {
+//     console.log("Sorry")
+// }
+// Example 6
+let name = "Saba";
+let age = 25;
+if (name == "Sana" && age == 25) {
+    console.log("Congratulations! You are eligible");
+}
+else {
+    console.log("Sorry");
+}
+export {};
